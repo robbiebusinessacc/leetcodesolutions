1f966d03d82b3bb28ee665d458cd4ea1aa293817
Robert Walmsley <65429016+robbiebusinessacc@users.noreply.github.com>
2022-06-14 17:23:20 +0000

    Sync LeetCode submission - Three Consecutive Odds (python3)

---
+++ my-folder/problems/three_consecutive_odds/solution.py
@@ -0,0 +1,8 @@
+class Solution:
+    def threeConsecutiveOdds(self,arr):
+        count=0
+        for i in arr:
+            if i%2==0:count=0
+            else:count+=1
+            if count == 3:return(True)
+        return(False)
