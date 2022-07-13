26951d9862fc8f0876bc1bdbb2965f5e2a343a92
Robert Walmsley <65429016+robbiebusinessacc@users.noreply.github.com>
2022-07-13 14:45:13 +0000

    Sync LeetCode submission - Divide Two Integers (python3)

--- my-folder/problems/divide_two_integers/solution.py
+++ my-folder/problems/divide_two_integers/solution.py
@@ -1,4 +1,3 @@
 class Solution:
-    def divide(self, dividend,divisor):
+    def divide(self, dividend: int, divisor: int) -> int:
         return min(int(str(dividend/divisor).split('.')[0]),2**31-1) 
-        
