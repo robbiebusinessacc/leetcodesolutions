59785083f337b6ae1a423b528f2face05750eea7
Robert Walmsley <65429016+robbiebusinessacc@users.noreply.github.com>
2022-08-08 15:11:00 +0000

    Sync LeetCode submission - Poor Pigs (python3)

---
+++ my-folder/problems/poor_pigs/solution.py
@@ -0,0 +1,6 @@
+class Solution:
+    def poorPigs(self, buckets: int, minutesToDie: int, minutesToTest: int) -> int:
+        return ceil(log(buckets)/log(minutesToTest/minutesToDie+1))
+        
+        
+        
