575f13db0281985c0945779cf2305405d2953d84
Robert Walmsley <65429016+robbiebusinessacc@users.noreply.github.com>
2023-02-13 19:57:37 +0000

    Sync LeetCode submission - Count Odd Numbers in an Interval Range (python3)

--- my-folder/problems/count_odd_numbers_in_an_interval_range/solution.py
+++ my-folder/problems/count_odd_numbers_in_an_interval_range/solution.py
@@ -1,4 +1,3 @@
 class Solution:
-	def countOdds(self,low,high):
-		if low%2==0 and high%2==0:return(high-low)//2
-		return(high-low)//2+1
+    def countOdds(self, low: int, high: int) -> int:
+        return(high+1)//2-low//2
