337c4068071a9801e137fc3902a70badc619711e
Robert Walmsley <65429016+robbiebusinessacc@users.noreply.github.com>
2023-10-09 01:29:33 +0000

    Sync LeetCode submission - N-Queens II (python3)

--- my-folder/problems/n-queens_ii/solution.py
+++ my-folder/problems/n-queens_ii/solution.py
@@ -1,10 +1,3 @@
 class Solution:
 	def totalNQueens(self,n):
-		if n==1:return 1
-		if n==2 or n==3:return 0
-		if n==4:return 2
-		if n==5:return 10
-		if n==6:return 4
-		if n==7:return 40
-		if n==8:return 92
-		if n==9:return 352
+		return[1,0,0,2,10,4,40,92,352][n-1]
